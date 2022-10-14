7d53e9e23c849de43482665932bd9138ebc082d4
Adigc7 <113979301+Adigc7@users.noreply.github.com>
2022-10-14 10:31:58 +0200

    Add files via upload

---
+++ Forms/binarioFusion.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html><head>
+</head><body>
+	
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"><h1>CONVERSOR BINARIO</h1>Numero decimal <input type='text' name='operando1' value='' size=15><br><br><input type="submit" value="enviar">
+<input type='reset' value="borrar">
+<br>
+<br>
+<br>
+</FORM>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $operando1 = test_input($_POST["operando1"]);
+   echo "El número decimal es: " .$operando1;
+   echo "<br>";	
+   $resultado = binario($operando1);	
+   echo "El númro binario es: " .$resultado;	
+   echo "<br>";}
+   
+function test_input($operando1) {
+  $operando1 = trim($operando1);
+  $operando1 = stripslashes($operando1);
+  $operando1 = htmlspecialchars($operando1);
+  return $operando1;
+}
+
+function binario($a) {
+    return decbin($a);
+}
+?>
+</body>
+</html>
+
+
+
