b3700e2f14c651b1198439124a442a57a35a4215
Adigc7 <113979301+Adigc7@users.noreply.github.com>
2022-10-24 10:32:36 +0200

    Add files via upload

---
+++ Files/fichero4.php
@@ -0,0 +1,38 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $nombre = test_input($_POST["nombre"]);
+  $apellido1 = test_input($_POST["apellido1"]);
+  $apellido2 = test_input($_POST["apellido2"]);
+  $fecha = test_input($_POST["fecha"]);
+  $localidad = test_input($_POST["localidad"]);
+
+
+
+$fichero = fopen ("alumnos2.txt","a") or die ("Fichero no creado");
+fwrite($fichero,str_pad($nombre,39,"#"));
+fwrite($fichero,str_pad($apellido1,40,"#"));
+fwrite($fichero,str_pad($apellido2,41,"#"));
+fwrite($fichero,str_pad($fecha,9,"#"));
+fwrite($fichero,str_pad($localidad,26,"#"));
+fwrite($fichero, "\n");
+		
+echo "<h1>Datos Alumnos</h1>";
+		echo "<table border=1>
+        <th>Nombre</th><th>Apellidos1</th><th>Apellidos2</th><th>Fecha</th><th>Localidad</th>";
+		echo "<tr><td>$nombre</td><td>$apellido1</td><td>$apellido2</td><td>$fecha</td><td>$localidad</td></tr>";
+
+        $archivo = "alumnos2.txt";
+        $lineas = count(file($archivo));
+        echo "<br>";
+        echo "El archivo alumno2.txt $archivo contiene $lineas lineas";
+};
+
+
+function test_input($data) {
+  $data = trim($data);
+  $data= stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+?>
