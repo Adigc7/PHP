6732b04a1e73c460a77e627f93ee115323b2b6d2
Adigc7 <113979301+Adigc7@users.noreply.github.com>
2022-10-26 12:11:51 +0200

    Update fichero4.php

--- Files/fichero4.php
+++ Files/fichero4.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $localidad = test_input($_POST["localidad"]);
 
 
-
+//Abrimos fichero, en caso de error avisar de que no se ha creado. Poner argumentos con su tamaño acompañado hasta siguiente con #, cerramos fichero.
 $fichero = fopen ("alumnos2.txt","a") or die ("Fichero no creado");
 fwrite($fichero,str_pad($nombre,39,"#"));
 fwrite($fichero,str_pad($apellido1,40,"#"));
@@ -15,19 +15,21 @@ fwrite($fichero,str_pad($apellido2,41,"#"));
 fwrite($fichero,str_pad($fecha,9,"#"));
 fwrite($fichero,str_pad($localidad,26,"#"));
 fwrite($fichero, "\n");
-		
+
+	//Creamos tabla con datos del fichero
 echo "<h1>Datos Alumnos</h1>";
 		echo "<table border=1>
         <th>Nombre</th><th>Apellidos1</th><th>Apellidos2</th><th>Fecha</th><th>Localidad</th>";
 		echo "<tr><td>$nombre</td><td>$apellido1</td><td>$apellido2</td><td>$fecha</td><td>$localidad</td></tr>";
-
+	
+	//Contamos filas del fichero y mostramos por pantalla
         $archivo = "alumnos2.txt";
         $lineas = count(file($archivo));
         echo "<br>";
         echo "El archivo alumno2.txt $archivo contiene $lineas lineas";
 };
 
-
+//Filtramos arguemntos de espacios, carcateres esppeciales y barra slash
 function test_input($data) {
   $data = trim($data);
   $data= stripslashes($data);
